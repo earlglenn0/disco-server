b5192b107c035c8ee86b4374e8733987162e5c04
garyamaro1 <gary@dnamicro.com>
2020-10-13 13:35:27 +0800

    interval to server

--- discoImplementation.js
+++ discoImplementation.js
@@ -6,6 +6,14 @@ const discoImplementation = {
   },
   guards: {
     hasReachedLimit: (context) => context.clicks >= context.max_clicks,
+  },
+  services: {
+    timer: (context, event) => send => {
+      const interval = setInterval(() => {
+        send('SEND_STATES')
+      }, 1000)
+      return () => clearInterval(interval)
+    }
   }
 }
 
