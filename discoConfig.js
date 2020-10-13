55b6e9171136ab0deffb30ea62815083cf50af6c
garyamaro1 <gary@dnamicro.com>
2020-10-13 13:33:34 +0800

    interval to server

--- discoConfig.js
+++ discoConfig.js
@@ -10,103 +10,112 @@ const discoConfig = {
       on: {
         TURN_ON: [
           { target: '#broken', cond: 'hasReachedLimit' },
-          { target: 'on', actions: 'incrementClick' }
+          { target: 'on.hist', actions: 'incrementClick' }
         ]
       }
     },
     on: {
       type: 'parallel',
-        on: {
-          TURN_OFF: [
-            { target: '#broken', cond: 'hasReachedLimit' },
-            { actions: 'incrementClick', target: 'off' }
-          ]
+      invoke: {
+        id: 'timer',
+        src: 'timer'
+      },
+      on: {
+        TURN_OFF: [
+          { target: '#broken', cond: 'hasReachedLimit' },
+          { actions: 'incrementClick', target: 'off' }
+        ],
+        SEND_STATES: {}
+      },
+      states: {
+        color: {
+          initial: 'steady',
+          states: {
+            steady: {
+              on: {
+                SET_COLOR_TO_CHANGING: [
+                  { target: '#broken', cond: 'hasReachedLimit', },
+                  { target: 'changing', actions: 'incrementClick', }
+                ],
+              }
+            },
+            changing: {
+              on: {
+                SET_COLOR_TO_STEADY: [
+                  { target: '#broken', cond: 'hasReachedLimit' },
+                  { target: 'steady', actions: 'incrementClick' }
+                ]
+              }
+            }
+          }
         },
-        states: {
-          color: {
-            initial: 'steady',
+        speed: {
+          initial: 'low',
+          states: {
+            low: {
+              on: {
+                SET_SPEED_TO_MEDIUM: [
+                  { target: '#broken', cond: 'hasReachedLimit' },
+                  { target: 'medium', actions: 'incrementClick' }
+                ],
+                SET_SPEED_TO_HIGH: [
+                  { target: '#broken',  cond: 'hasReachedLimit' },
+                  { target: 'high', actions: 'incrementClick' }
+                ]
+              }
+            },
+            medium: {
+              on: {
+                SET_SPEED_TO_LOW: [
+                  { target: '#broken', cond: 'hasReachedLimit' },
+                  { target: 'low', actions: 'incrementClick' }
+                ],
+                SET_SPEED_TO_HIGH: [
+                  { target: '#broken', cond: 'hasReachedLimit' },
+                  { target: 'high', actions: 'incrementClick' }
+                ]
+              }
+            },
+            high: {
+              on: {
+                SET_SPEED_TO_LOW: [
+                  { target: '#broken', cond: 'hasReachedLimit' },
+                  { target: 'low', actions: 'incrementClick' }
+                ],
+                SET_SPEED_TO_MEDIUM: [
+                  { target: '#broken', cond: 'hasReachedLimit' },
+                  { target: 'medium', actions: 'incrementClick' }
+                ]
+              }
+            }
+          }
+        },
+        lightMode: {
+          initial: 'steady',
             states: {
               steady: {
                 on: {
-                  SET_COLOR_TO_CHANGING: [
-                    { target: '#broken', cond: 'hasReachedLimit', },
-                    { target: 'changing', actions: 'incrementClick', }
+                  SET_LIGHTMODE_TO_FLASHING: [
+                    { target: '#broken', cond: 'hasReachedLimit' },
+                    { target: 'flashing',  actions: 'incrementClick' }
                   ],
                 }
               },
-              changing: {
+              flashing: {
                 on: {
-                  SET_COLOR_TO_STEADY: [
+                  SET_LIGHTMODE_TO_STEADY: [
                     { target: '#broken', cond: 'hasReachedLimit' },
                     { target: 'steady', actions: 'incrementClick' }
-                  ]
-                }
-              }
-            }
-          },
-          speed: {
-            initial: 'low',
-            states: {
-              low: {
-                on: {
-                  SET_SPEED_TO_MEDIUM: [
-                    { target: '#broken', cond: 'hasReachedLimit' },
-                    { target: 'medium', actions: 'incrementClick' }
                   ],
-                  SET_SPEED_TO_HIGH: [
-                    { target: '#broken',  cond: 'hasReachedLimit' },
-                    { target: 'high', actions: 'incrementClick' }
-                  ]
                 }
               },
-              medium: {
-                on: {
-                  SET_SPEED_TO_LOW: [
-                    { target: '#broken', cond: 'hasReachedLimit' },
-                    { target: 'low', actions: 'incrementClick' }
-                  ],
-                  SET_SPEED_TO_HIGH: [
-                    { target: '#broken', cond: 'hasReachedLimit' },
-                    { target: 'high', actions: 'incrementClick' }
-                  ]
-                }
-              },
-              high: {
-                on: {
-                  SET_SPEED_TO_LOW: [
-                    { target: '#broken', cond: 'hasReachedLimit' },
-                    { target: 'low', actions: 'incrementClick' }
-                  ],
-                  SET_SPEED_TO_MEDIUM: [
-                    { target: '#broken', cond: 'hasReachedLimit' },
-                    { target: 'medium', actions: 'incrementClick' }
-                  ]
-                }
-              }
             }
-          },
-          lightMode: {
-            initial: 'steady',
-              states: {
-                steady: {
-                  on: {
-                    SET_LIGHTMODE_TO_FLASHING: [
-                      { target: '#broken', cond: 'hasReachedLimit' },
-                      { target: 'flashing',  actions: 'incrementClick' }
-                    ],
-                  }
-                },
-                flashing: {
-                  on: {
-                    SET_LIGHTMODE_TO_STEADY: [
-                      { target: '#broken', cond: 'hasReachedLimit' },
-                      { target: 'steady', actions: 'incrementClick' }
-                    ],
-                  }
-                },
-              }
-          }
+        },
+        hist: {
+          type: 'history',
+          history: 'deep'
         }
+      }
     },
     broken: {
       id: 'broken',
