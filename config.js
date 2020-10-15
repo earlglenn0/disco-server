64cb57c83c6789a59b0c4df2256ae333a20561dd
garyamaro1 <gary@dnamicro.com>
2020-10-15 11:32:18 +0800

    kafka

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  kafka_topic_produce: 'machine-input',
+  kafka_topic_consume: 'machine-changes',
+  kafka_server: 'localhost:9092'
+}
--- index.js
+++ index.js
@@ -1,42 +1,51 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const { Machine, interpret } = require('xstate')
-const discoConfig = require('./discoConfig')
-const discoImplementation = require('./discoImplementation')
-const { formatState } = require('./utils')
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+const config = require('./config')
+const kafka = require('kafka-node')
+const Producer = kafka.Producer
+const client = new kafka.KafkaClient(config.kafka_server)
+const producer = new Producer(client)
+const Consumer = kafka.Consumer
+const consumer = new Consumer(client, [{ topic: config.kafka_topic_consume, partition: 0 }], { autoCommit: false })
 
-io.on('connection', (socket) => {
-  console.log('user connected');
-  const discoMachine = Machine(discoConfig, discoImplementation)
-  const service = interpret(discoMachine)
-  service.start()
-  service.onTransition(state => {
-    io.emit('STATE_CHANGED', {
-      ...formatState(state.value),
-      ...state.context,
-      isBroken: state.value === 'broken'
+const events = [
+  'TURN_ON', 'TURN_OFF',
+  'SET_COLOR_TO_CHANGING', 'SET_COLOR_TO_STEADY',
+  'SET_SPEED_TO_LOW', 'SET_SPEED_TO_MEDIUM', 'SET_SPEED_TO_HIGH',
+  'SET_LIGHTMODE_TO_STEADY', 'SET_LIGHTMODE_TO_FLASHING'
+]
+
+producer.on('ready', () => {
+  console.log('producer ready')
+  io.on('connection', (socket) => {
+    console.log('user connected')
+    events.forEach(event => {
+      socket.on(event, () => {
+        const payloads = [
+          {
+            topic: config.kafka_topic_produce,
+            messages: [event]
+          }
+        ]
+        producer.send(payloads, (err, data) => {
+          if (err) {
+            console.log('producer failed: ', event)
+          } else {
+            console.log('producer succeeded: ', event)
+          }
+        })
+      })
     })
-  })
-
-  const events = [
-    'TURN_ON', 'TURN_OFF',
-    'SET_COLOR_TO_CHANGING', 'SET_COLOR_TO_STEADY',
-    'SET_SPEED_TO_LOW', 'SET_SPEED_TO_MEDIUM', 'SET_SPEED_TO_HIGH',
-    'SET_LIGHTMODE_TO_STEADY', 'SET_LIGHTMODE_TO_FLASHING'
-  ]
-
-  events.forEach(event => {
-    socket.on(event, () => {
-      service.send(event)
+    consumer.on('message', (message) => {
+      console.log({ message })
+      io.emit('STATE_CHANGED', JSON.parse(message.value))
     })
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
   })
-  
-  socket.on('disconnect', () => {
-    service.stop()
-    console.log('user disconnected');
-  });
-});
+})
 
 http.listen(3001, () => {
   console.log('listening on *:3001');
