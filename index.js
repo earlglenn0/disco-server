14d2d8fb739d88e6d06fc405e77e047092694fe4
garyamaro1 <gary@dnamicro.com>
2020-10-16 13:56:22 +0800

    Revert "kafkajs"
    This reverts commit 8bd200cbfd8273d575ac4e7e3fe243fb944612f7.

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
-
 const config = require('./config')
-const { Kafka } = require('kafkajs')
-const kafka = new Kafka({
-  brokers: [config.kafka_server]
+const kafka = require('kafka-node')
+const Producer = kafka.Producer
+const client = new kafka.KafkaClient(config.kafka_server)
+const producer = new Producer(client)
+const Consumer = kafka.Consumer
+const consumer = new Consumer(client, [{ topic: config.kafka_topic_consume, partition: 0 }], {
+  autoCommit: true,
+  fetchMaxWaitMs: 1000,
+  fetchMaxBytes: 1024 * 1024,
+  encoding: 'utf8',
+  fromOffset: false
 })
-const producer = kafka.producer()
-const consumer = kafka.consumer({ groupId: 'test-consumer-group' })
 
 const events = [
   'TURN_ON', 'TURN_OFF',
@@ -17,33 +22,40 @@ const events = [
   'SET_LIGHTMODE_TO_STEADY', 'SET_LIGHTMODE_TO_FLASHING'
 ]
 
-const run = async () => {
-  await producer.connect()
+http.listen(3001, () => {
+  console.log('listening on *:3001');
 
-  io.on('connection', (socket) => {
-    console.log('user connected')
-    events.forEach(event => {
-      socket.on(event, async () => {
-        await producer.send({
-          topic: config.kafka_topic_produce,
-          messages: [{ value: event }]
+  const produceEvent = (topic, payload) => {
+    const payloads = [{
+      topic,
+      messages: [ payload]
+    }]
+    producer.send(payloads, (err, data) => {
+      if (err) {
+        console.log('producer failed')
+      } else {
+        console.log('producer succeeded')
+      }
+    })
+  }
+  
+  producer.on('ready', () => {
+    console.log('producer ready')
+    io.on('connection', (socket) => {
+      console.log('user connected')
+      events.forEach(event => {
+        socket.on(event, () => {
+          produceEvent(config.kafka_topic_produce, event)
         })
       })
+      socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
     })
-    socket.on('disconnect', () => {
-      console.log('user disconnected')
-    })
-  })
-
-  await consumer.connect()
-  await consumer.subscribe({ topic: config.kafka_topic_consume })
-  await consumer.run({
-    eachMessage: async ({ message }) => {
+    consumer.on('message', (message) => {
+      console.log({ message })
       io.emit('STATE_CHANGED', JSON.parse(message.value))
-    }
+    })
   })
-}
-
-http.listen(3001, () => {
-  run().catch(console.log)
-})
+  
+});
